16a22af530af6a85d29548f5eb8b70eb1fd0324e
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-08 14:52:46 +0300

    добавил решение задачи 4.2

--- Lab4.2/DoWhile.cs
+++ Lab4.2/DoWhile.cs
@@ -10,7 +10,25 @@ namespace Lab4._2
     {
         static void Main(string[] args)
         {
-             
+            double n;
+            int a = 0;
+            int b = 0;
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                {
+                    a++;
+                }
+                else if (n < 0)
+                {
+                    b++;
+                }
+            } while ( n != 0);
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+
+            Console.ReadKey();
         }
     }
 }
