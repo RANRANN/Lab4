8ef802498c72e46df27d82b812b06549b92aae4e
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-08 12:13:15 +0300

    Добавьте файлы проекта.

---
+++ Lab4.3/while.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A = ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C = ");
+            int C = Convert.ToInt32(Console.ReadLine());
+            int S1 = A * B;
+            Console.WriteLine("S1 = {0}",S1);
+            int S2 = C * C;
+            Console.WriteLine("S2 = {0}",S2);
+            int n = 0;
+            while (S1 - S2 >= 0)
+            {
+                S1 -= S2;
+                n++;
+            }
+            Console.WriteLine("n = {0}", n);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ for.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" N = ");
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            for (uint i = 1, j = 0; i <= N; i++)
+            {
+                Console.WriteLine(j += 2 * i - 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
